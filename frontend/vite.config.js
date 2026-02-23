d90fe706cc1f2dd2ab900477d9fde6e96b53909d
francoschiavoni <francoschiavo7@gmail.com>
2026-02-23 17:55:06 -0300

    fix railway

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    // Esto le dice a Vite que permita el tráfico desde tu dominio de Railway
+    allowedHosts: ['dim-sistema-frontend-production.up.railway.app'],
+  }
 })
