c4eafc49a9d0334f5ba00d5d5f04d5b0a53edecc
fschiavoni <francoschiavo7@gmail.com>
2026-02-25 10:05:32 -0300

    host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   plugins: [react()],
   preview: {
     // Esto le dice a Vite que permita el tráfico desde tu dominio de Railway
-    allowedHosts: ['dim-sistema-web.up.railway.app'],
+    allowedHosts: ['dim-sistema-web.up.railway.app', 'dim-sistema-frontend-staging.up.railway.app'],
   }
 })
